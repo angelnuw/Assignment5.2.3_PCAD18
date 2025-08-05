9751f9f8f88bf39f57d50c1d45e63a8fcefca89e
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-05 17:00:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment5._2._3
+{
+    internal class Program
+    {
+        static void Number_TailRecursion(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(n + " ");
+                Number_TailRecursion(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Number_TailRecursion(10);
+            Console.ReadKey();
+        }
+    }
+}
